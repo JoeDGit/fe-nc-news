e4b913fa1dce9d48d729c7ef614540e3ba330273
JoeDGit <joe.dobbins89@gmail.com>
2023-03-23 01:28:55 +0000

    removes console log

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 export default function Button({ text, display }) {
-  console.log(display);
   return (
     <button
       className={`${display} active:translate-y-0.5 active:translate-x-0.5 mx-2 mb-2 py-2 px-2 rounded bg-[#FF4500]`}
