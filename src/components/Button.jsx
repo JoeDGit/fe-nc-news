795719a4f21d6a8666e0b3353fbdc83f8677997f
JoeDGit <joe.dobbins89@gmail.com>
2023-03-25 22:57:29 +0000

    Adds ubmit type to button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 export default function Button({ text, display }) {
   return (
     <button
+      type="submit"
       className={`${display} active:translate-y-0.5 active:translate-x-0.5 mx-2 mb-2 py-2 px-2 rounded bg-primary`}
     >
       {text}
