c2eb8060c422e0cf79533f05c1a9b289548eed0f
JoeDGit <joe.dobbins89@gmail.com>
2023-03-26 02:15:59 +0100

    adds textSize prop to button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-export default function Button({ text, display }) {
+export default function Button({ text, textSize, display }) {
   return (
     <button
       type="submit"
-      className={`${display} active:translate-y-0.5 active:translate-x-0.5 mx-2 mb-2 py-2 px-2 rounded bg-primary`}
+      className={`${display} ${textSize} active:translate-y-0.5 active:translate-x-0.5 mx-2 mb-2 py-2 px-2 rounded bg-primary`}
     >
       {text}
     </button>
