5e5bb6e67a0704b23db21f7d9bcf79704066c1bf
JoeDGit <joe.dobbins89@gmail.com>
2023-03-27 01:09:39 +0100

    adds onClick handler to button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 
-export default function Button({ text, textSize, display }) {
+export default function Button({ text, textSize, display, onClick }) {
   return (
     <button
+      onClick={onClick}
       type="submit"
       className={`${display} ${textSize} active:translate-y-0.5 active:translate-x-0.5 mx-2 mb-2 py-2 px-2 rounded bg-primary`}
     >
