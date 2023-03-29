b5d589d1df4922b207bef183ba297c53540dc029
JoeDGit <joe.dobbins89@gmail.com>
2023-03-29 02:38:57 +0100

    adds bad path styling

--- src/components/BadPath.jsx
+++ src/components/BadPath.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
-
+import badpath from '../assets/404.png';
 export default function BadPath() {
-  return <h2>404 - Path not found</h2>;
+  return (
+    <div>
+      <h2 className="text-3xl">404 - Path not found</h2>
+      <div className="flex justify-center mt-10">
+        <img src={badpath} alt="bad path"></img>
+      </div>
+    </div>
+  );
 }
